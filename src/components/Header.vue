2fc5ff9253e14895ddb2401f02d805722d0ffa48
Michel Souza <michelnsouza98@gmail.com>
2022-05-21 10:25:46 -0300

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+<h1>
+    <strong class="uni">Un1</strong><strong class="games">Games</strong> - Adivinhe o número
+</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+div{
+    text-align: center;
+    background-color: white;
+    width: 100%;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);   
+    padding-top: 26px ;
+    padding-bottom: 29px;
+}
+
+h1{
+    font-family: 'Inter';
+    text-align: center;
+    size: 46px;
+}
+    .uni{
+        color:#D84E41;
+    }
+    .games{
+        color: rgb(245, 87, 30);
+    }
+</style>
+    
